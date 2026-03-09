279a9946d48a8a049e81b0e1c568806012d5745d
larissackk <larissa.alcantara.gomes@escola.pr.gov.br>
2026-03-09 08:57:02 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,13 @@
+console.log("Olá mundo")
+//comentario
+// ctrl para salvar
+
+
+
+
+let nome = "larissa"
+let altura = 1.70
+let idade = 16
+let roupa = "uniforme escolar"
+
+console.log("Olá, meu nome é " + nome +" possuo " + idade + " anos de idade " + " tenho de altura " + altura + " estou uasando o " + roupa +" do coleégio Kennedy." )
