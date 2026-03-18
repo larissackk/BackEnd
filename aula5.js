e70e2942aa8652141bb823239288efc3e961983f
larissackk <larissa.alcantara.gomes@escola.pr.gov.br>
2026-03-18 09:39:24 -0300

    Add files via upload

---
+++ aula5.js
@@ -0,0 +1,11 @@
+ const alunos = ["Amanda" , "Ariel" , "Carlos" ]
+ const altura = [ 1.51 , 1.80 , 1.74 ]
+ 
+
+ let computador = 0 //variavel de inicio 
+ while (contador <= 2 ){ //condiçao de parada 
+  console.log(alunos[contador] + "-"  + altura[conatador])
+  contador = conatdor + 1 // incremento de laço 
+
+
+ } 
