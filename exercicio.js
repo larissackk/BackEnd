7508ab036b6a6dc4ce07e3bca719f79c91ae8e1d
larissackk <larissa.alcantara.gomes@escola.pr.gov.br>
2026-03-23 08:58:34 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,18 @@
+// crie o nome de 10 produtos e armazene em uma lista 
+
+const produto1 = 
+const produto2 = const produto = 
+
+const produto3 = 
+const produto4 =
+const produto5 =
+const produto6 =
+const produto9 =
+const produto8 =
+const produto9 =
+const produto10 =
+
+
+
+const produto = "Arroz Branco ( pacote de 5kg)" , Feijão Carioca ( pacote de 1kg)" 
+
