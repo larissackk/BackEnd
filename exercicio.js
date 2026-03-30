61ec077ee08f17b3b2e655d85bf380008feff625
larissackk <larissa.alcantara.gomes@escola.pr.gov.br>
2026-03-30 08:13:20 -0300

    Add files via upload

--- exercicio.js
+++ exercicio.js
@@ -1,18 +1,39 @@
-// crie o nome de 10 produtos e armazene em uma lista 
-
-const produto1 = 
-const produto2 = const produto = 
-
-const produto3 = 
-const produto4 =
-const produto5 =
-const produto6 =
-const produto9 =
-const produto8 =
-const produto9 =
-const produto10 =
+const produtos = ["Arroz Integral", "Café", "Feijão Carioca", "Açúcar mascavo", "Leite", "Óleo", "Ovos", "Pão", "Manteiga", "Chocolate"]
+const valores = ["R$ 28,00", "R$ 8,50", "R$ 16,00", "R$ 4,50", "R$ 6,50", "R$ 5,00", "R$ 18,00", "R$ 9,00", "R$ 12,00", "R$ 22,00"]
+const estoque = ["7 unidades", "4 unidades", "10 unidade", "6 unidades", "3 unidades", "12 unidades", "24 unidades", "20 unidades", "6 unidades", "10 unidades"]
 
 
+function mostrarESTOQUE() {
+    let contador = 0
+  while(contador <produtos.length){
+      console.log(produtos[contador] + " - " + valores[contador] + " - " + estoque[contador])
+contador++
+  }
+}
 
-const produto = "Arroz Branco ( pacote de 5kg)" , Feijão Carioca ( pacote de 1kg)" 
+function adicionarPRODUTO() {
+    produtos.push("Papel Hgienioco")
+    valores.push("R$ 17,00")
+    estoque.push("23 unidades")
+}
 
+function remorerPRODUTO(){
+    produtos.splice(10, 1)
+    valores.splice(10, 1)
+    estoque.splice(10, 1)
+}
+
+
+mostrarESTOQUE()
+
+adicionarPRODUTO()
+
+console.log("-----------------------------------")
+
+mostrarESTOQUE()
+
+remorerPRODUTO()
+
+console.log("-----------------------------------")
+
+mostrarESTOQUE()
